e82a0914546391e26faefbe4a7f9c1da5006f24f
sy0201 <noey0201@gmail.com>
2023-06-26 15:48:58 +0900

    함수 종료되지 않게 수정

--- MyCreditManager_0515/main.swift
+++ MyCreditManager_0515/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 var studentList = [Student]()
-
+var dic: [String:Student] = [:]
 
 func creditManager() {
     print("원하는 기능을 입력해주세요.")
@@ -57,14 +57,15 @@ func addStudent() {
     guard let name = name,!name.isEmpty else { return }
 
     if studentList.contains(where: { $0.name == name }) {
-        return print("\(name)이 이미 존재하는 학생입니다. 추가하지 않습니다.")
+        print("-->>\(studentList.contains(where: { $0.name == name }))")
+        print("\(name)이 이미 존재하는 학생입니다. 추가하지 않습니다.")
+    } else {
+
+        let student = Student(name: name)
+        studentList.append(student)
+
+        print("\(name) 학생을 추가했습니다.")
     }
-
-    var student = Student()
-    student.name = name
-    studentList.append(student)
-
-    print("\(name) 학생을 추가했습니다.")
     creditManager()
 }
 
@@ -86,12 +87,10 @@ func removeStudent() {
         // 입력된 name 있으니 배열에서 삭제
         studentList[0].name
         print("\(String(describing: studentList[0].name))학생을 삭제하였습니다.")
-        
         studentList = studentList.filter {
             return $0.name != name
         }
     }
-
     print("-->> \(studentList)")
               
     creditManager()
@@ -103,19 +102,18 @@ func addGrade() {
 
     guard let inputText = inputText,!inputText.isEmpty else { return }
 
-    let splitedInputText = inputText.split(separator: " ")
+    var splitedInputText = inputText.split(separator: " ")
 
     if splitedInputText.count != 3 {
         print("입력이 잘못되었습니다. 다시 확인해주세요.")
-        return
     }
     print(splitedInputText)
     print(inputText)
-    
+
     let name = String(splitedInputText[0])
     let subject = String(splitedInputText[1])
     let grade = String(splitedInputText[2])
-    
+
     switch grade {
     case "A+":
         print(4.5)
@@ -141,9 +139,10 @@ func addGrade() {
     print(name, subject, grade)
     
     // 입력된 학생성적 배열에 추가
-    var setStudent = Student(name: String(name), subject: String(subject), grade: Double(grade))
-    print("\(name)학생의 \(subject)과목이 \(grade)로 변경되었습니다")
-    
+    var addStudent = Student(name: String(name), subject: String(subject), grade: Double(grade))
+    studentList.append(addStudent)
+
+    print("\(name)학생의 \(subject)과목이 \(grade)로 추가(변경) 되었습니다")
     creditManager()
 }
 
@@ -156,16 +155,17 @@ func deleteGrade() {
     }
     
     let splitedInputText = inputText.split(separator: " ")
-    
     if splitedInputText.count != 2 {
         print("입력이 잘못되었습니다.다시 확인해주세요")
     }
-        
-    // 이름을 기준으로 이름과 과목을 찾아서 성적 삭제
+
     let name = splitedInputText[0]
     let subject = splitedInputText[1]
-
     print(splitedInputText)
+
+    // 이름을 기준으로 이름과 과목을 찾아서 성적 삭제
+
+    let removeStudentGrade = Student(name: String(name), subject: String(subject), grade: nil)
     print("\(name)학생의 \(subject)과목의 성적이 삭제되었습니다.")
     
     creditManager()
@@ -178,13 +178,16 @@ func average() {
     guard let name = name,!name.isEmpty else { return }
     
     if studentList.contains(where: { $0.name == name} ) {
-        // 이름을 기준으로 subject와 grade 출력
-        
-        print("==>>")
+        // 이름을 기준으로 리스트에서 subject와 grade 출력
+        //print("\(subject): \(grade)")
+
+        // 평점 보여주기
+    } else {
+        //이름 검색 후 없으면
+        let name = Student().name
+        print("\(String(describing: name))학생을 찾을 수 없습니다.")
     }
-    
-    //이름 검색 후 없으면
-    print("\(name)학생을 찾을 수 없습니다.")
+
     creditManager()
 }
 
